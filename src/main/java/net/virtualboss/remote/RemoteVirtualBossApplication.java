6901ca936b27cf822847f2ec305493fadd6b0b36
Sergei Ragaev <sergei.ragaev@gmail.com>
2024-10-30 13:36:55 +0200

    initial commit

---
+++ src/main/java/net/virtualboss/remote/RemoteVirtualBossApplication.java
@@ -0,0 +1,13 @@
+package net.virtualboss.remote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteVirtualBossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteVirtualBossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/virtualboss/remote/RemoteVirtualBossApplicationTests.java
@@ -0,0 +1,13 @@
+package net.virtualboss.remote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteVirtualBossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
